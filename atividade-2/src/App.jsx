13a426b475b708084b438c0e8adf7aa90acd6260
quezcst <154010743+quezinhacosta@users.noreply.github.com>
2026-03-10 12:13:35 +0000

    nova atividade

---
+++ atividade-2/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>atividade</h1>
+      <h2>Fundo e Bordas / Margem, padding, largura e altura / Fontes / Transp.</h2>
+      <hr></hr>
+      <br></br>
+
+      <div className="botoes">
+        <a href="210.js">atividade 210</a>
+        <a href="231.js">atividade 231</a>
+        <a href="246.js">atividade 246</a>
+        <a href="263.js">atividade 263</a>
+      </div>
+      <br>
+      </br>
+      <hr></hr>
+
+      <footer>feito integralmente pela aluna <strong> Quezia Costa</strong> </footer>
+    </>
+  )
+}
+
+export default App
